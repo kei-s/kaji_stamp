2395597b696faddfcbb3bde1bb6cf007b15ba19f
kei <kei.shiratsuchi@gmail.com>
2013-09-15 23:47:52 +0900

    Initail commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+KajiStamp::Application.config.secret_key_base = '19f1112bc1b871178022dc44a338e641ed8c642af8e62d7d2ebcfac67a178ef4958c21e298f608cfbdbc05409df85e5dfa7c4165dcf005ccb72c43a338ed1eb8'
---
+++ config/initializers/session_store.rb
@@ -0,0 +1,3 @@
+# Be sure to restart your server when you modify this file.
+
+KajiStamp::Application.config.session_store :cookie_store, key: '_kaji_stamp_session'
